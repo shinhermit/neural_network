294e3bd4ceb42eb5ceda0e8afb4bec61cc813fa3
Josuah Aron <josuah@josuah-K43SJ.(none)>
2012-08-20 16:10:47 +0200

    file i forgot to add in last commit

---
+++ test_synaptic.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include "synaptic.hpp"
+
+using namespace std;
+
+int main(){
+  int i, layer, position;
+  unit src, dst;
+  synaptic synapses[3];
+
+  //asking data for 3 connections to the user
+  cout<<endl<<"Creation of 3 connections..."<<endl;
+  for(i=0; i<3; i++){
+    cout<<"connection "<<i+1<<endl;
+
+    cout<<"layer where the source unit is: ";
+    cin>>layer;
+    cout<<"position in the layer: ";
+    cin>>position;
+    src = unit(layer, position);
+
+    cout<<"layer where the destination unit is: ";
+    cin>>layer;
+    cout<<"position in the layer: ";
+    cin>>position;
+    dst = unit(layer, position);
+
+    synapses[i] = synaptic(src, dst);
+  }
+
+  cout<<endl<<"Comparing the connections..."<<endl;
+  cout<<"synapse1 == synapse2 : ";
+  if(synapses[0] == synapses[1])
+    cout<<"true"<<endl;
+  else
+    cout<<"false"<<endl;
+
+  cout<<"synapse1 == synapse3 : ";
+  if(synapses[0] == synapses[2])
+    cout<<"true"<<endl;
+  else
+    cout<<"false"<<endl;
+
+  cout<<"synapse2 == synapse3 : ";
+  if(synapses[1] == synapses[2])
+    cout<<"true"<<endl;
+  else
+    cout<<"false"<<endl;
+
+  cout<<endl<<"printing synapses ..."<<endl;
+  cout<<"1: "<<synapses[0]<<endl;
+  cout<<"2: "<<synapses[1]<<endl;
+  cout<<"3: "<<synapses[2]<<endl;
+
+  cout<<endl<<"unsing layer(), source() and dest() observers ..."<<endl;
+  cout<<"1: layer() -> "<<synapses[0].layer()<<" | source() -> "<<synapses[0].source()<<" | dest() -> "<<synapses[0].dest()<<endl;
+
+  return 0;
+}
---
+++ test_unit.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "unit.hpp"
+
+using namespace std;
+
+int main(){
+  int i, layer, position;
+  unit units[3];
+
+  //asking data for 3 units to the user
+  cout<<endl<<"Creation of 3 units..."<<endl;
+  for(i=0; i<3; i++){
+    cout<<"layer where the unit "<<i+1<<" is: ";
+    cin>>layer;
+    cout<<"position in the layer: ";
+    cin>>position;
+    units[i] = unit(layer,position);
+  }
+
+  cout<<endl<<"Comparing the units..."<<endl;
+  cout<<"unit1 == unit2 : ";
+  if(units[0] == units[1])
+    cout<<"true"<<endl;
+  else
+    cout<<"false"<<endl;
+
+  cout<<"unit1 == unit3 : ";
+  if(units[0] == units[2])
+    cout<<"true"<<endl;
+  else
+    cout<<"false"<<endl;
+
+  cout<<"unit2 == unit3 : ";
+  if(units[1] == units[2])
+    cout<<"true"<<endl;
+  else
+    cout<<"false"<<endl;
+
+  cout<<endl<<"printing units ..."<<endl;
+  cout<<"1: "; units[0].print(); cout<<endl;
+  cout<<"2: "; units[1].print(); cout<<endl;
+  cout<<"3: "<<units[2]<<endl;
+
+  cout<<endl<<"unsing layer() and pos() observers ..."<<endl;
+  cout<<"1: layer() -> "<<units[0].layer()<<" | pos() -> "<<units[0].pos()<<endl;
+
+  return 0;
+}
